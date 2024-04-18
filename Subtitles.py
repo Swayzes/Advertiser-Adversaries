9e4efb6852024a654c42dd51297099a121ed0711
SomeGuyEatingPie <94908713+SomeGuyEatingPie@users.noreply.github.com>
2024-04-18 12:09:27 +0100

    Subtitles class

--- Subtitles.py
+++ Subtitles.py
@@ -1,3 +1,24 @@
+"""
+Subtitle anlysis
+author: Sean Johnson
+"""
+
 from textblob import TextBlob
 # pip install -U textblob
 # python -m textblob.download_corpora
+import pysubs2
+#pip install pysubs2
+class subttiles():
+
+    def  subreader(vID, 
+                path = "dataset/subtitles", 
+                lang = "en", 
+                ft = "vtt"):
+        
+        subs = pysubs2.load(f"{path}/{vID}.{lang}.{ft}")
+        print(subs)
+
+    #def nextSubtitle()
+
+subttiles.subreader("VdOlqcg9uMQ")
+pass
