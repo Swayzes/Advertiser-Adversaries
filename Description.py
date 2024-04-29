489c571c0d8c52ec01685eade5ac106a851216e5
Swayzes <56790526+Swayzes@users.noreply.github.com>
2024-04-29 15:35:39 +0100

    Update Description.py

--- Description.py
+++ Description.py
@@ -3,11 +3,13 @@ import io
 from pathlib import Path
 import os
 # https://scikit-learn.org/stable/modules/feature_extraction.html
-
+import json
 # pip install -U scikit-learn
 import sklearn
 from sklearn.feature_extraction.text import CountVectorizer
 from transformers import BertTokenizer, BertModel
+
+encoded_text = {"data":[], "labels":[]}
 
 def BoW_Processing(text):
     # create the vocabulary
@@ -24,14 +26,15 @@ def BoW_Processing(text):
     print(bow_model)
     return
 
-def BERT_Processing(text):
+def BERT_Processing(text, label):
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     model = BertModel.from_pretrained("bert-base-uncased")
-    encoded_input = tokenizer(text, return_tensors='pt')
-    tokens = tokenizer.convert_ids_to_tokens(encoded_input)
+    encoded_input = tokenizer(text, return_tensors='pt', max_length=500)
+    # tokens = tokenizer.convert_ids_to_tokens(encoded_input)
     output = model(**encoded_input)
-    print("BERT")
-    print()
+    output.last_hidden_state.mean(dim=1).squeeze().detach().cpu().numpy()
+    encoded_text["data"].append(output)
+    encoded_text["labels"].append(label)
     return
 
 def desc_processing(desc, label):
@@ -66,16 +69,27 @@ for d in descListAds:
 for d in descListNoAds:
     descList.append(desc_processing(d, 0))
 
-print(descList)
-
+# print(descList)
+BERT_Processing(descList[0][0], descList[0][1])
 # for d in descList:
-    # BoW_Processing(description)
-    # BERT_Processing(description)
+#     # BoW_Processing(description)
+#     BERT_Processing(d[0], d[1])
 
 # Load up a bunch of video data to make a dataset
 # Create a dataframe with all the description based data
 # Aspect Extraction:
 # Use Bag of Words?
 # Hashing is a thing?
-# Sentiment Analysis:
-# 
+# Sentiment Analysis
+
+# SVM, gradient boosting
+print(encoded_text)
+
+def save(dir, name):
+    if(not os.path.exists(dir)):
+        os.makedirs(dir)
+    path = os.path.join(dir, name)
+    with open(path, "w") as f:
+        json.dump(encoded_text, f, indent=4)
+
+save("encoded_description", "training_data_all.json")
