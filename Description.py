4cc3ad632f16817a6ba1ac79e06bfbd185659e1d
Swayzes <56790526+Swayzes@users.noreply.github.com>
2024-04-30 23:45:58 +0100

    polish up

--- Description.py
+++ Description.py
@@ -7,8 +7,7 @@ import io
 from pathlib import Path
 import os
 import numpy as np
-import sklearn
-import torch
+import pickle
 import pandas as pd
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, f1_score
@@ -17,30 +16,17 @@ from sklearn.feature_extraction.text import CountVectorizer
 from transformers import BertTokenizer, BertModel
 from sklearn.model_selection import train_test_split
 
-# encoded_text = {"data":[], "labels":[]}
 data = []
 labels =[]
 combined = pd.DataFrame()
 
-def BoW_Processing(text):
-    """
-    
-    """
-    # Create the vocabulary
-    vectorizer = CountVectorizer()
-
-    # fit the vocabulary to the text data
-    vectorizer.fit(text)
-
-    # create the bag-of-words model
-    bow_model = vectorizer.transform(text)
-
-    # print the bag-of-words model
-    print("BoW")
-    print(bow_model)
-    return
-
+## Predicts if a description has a sponsor or not
 def BERT_Processing(text, label):
+    """
+    Processes the description text into BERT to encode the words
+    param text: The body of text of the description
+    param label: The label assigned
+    """
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     model = BertModel.from_pretrained("bert-base-uncased")
     encoded_input = tokenizer(text, return_tensors='pt', max_length=500, truncation = True)
@@ -48,40 +34,38 @@ def BERT_Processing(text, label):
     output = model(**encoded_input)
     output = output.last_hidden_state.mean(dim=1).squeeze().detach().cpu().numpy()
 
-    # encoded_text["data"].append(output)
-    # encoded_text["labels"].append(label)
-
     data.append(output)
     labels.append(label)
     return
 
 def desc_processing(desc, label):
+    """
+    Processes the dataset with no labels
+    param desc: The path to the description
+    param label: The assigned label
+    """
     text = open(Path(desc[0]), encoding="utf8")
     description = [text.read()]
 
     description.append(label)
     return description
 
-class NumpyTypeEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.generic):
-            return obj.item()
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        return json.JSONEncoder.default(self, obj)
-    
 def save(dir, name):
+    """
+    Saves the processed datasets into JSON
+    param dir: The directory
+    param name: The name of the file
+    """
+
     if(not os.path.exists(dir)):
         os.makedirs(dir)
     path = os.path.join(dir, name)
 
-    # json_data = json.dumps(encoded_text, indent=4, cls=NumpyTypeEncoder)
-    # with open(path, "w") as f:
-    #         f.write(json_data)
     combined = pd.DataFrame({
     "data": data,
     "labels": labels
     })
+
     output = combined.to_json(orient="records", lines=True)
     with open(path, "w") as f: # Change as needed
         f.write(output)
@@ -90,6 +74,7 @@ con = sqlite3.connect("VideoDatabase.db")
 cur = con.cursor()
 
 def Training_processing():
+    """Processes the Training dataset"""
     descList = []
     cur.execute("SELECT Description_File_Path FROM DatasetAds")
     descListAds = cur.fetchall()
@@ -103,13 +88,13 @@ def Training_processing():
         descList.append(desc_processing(d,0))
 
     for d in descList:
-        # BoW_Processing(description)
         BERT_Processing(d[0], d[1])
 
     save("encoded_description", "BERT_training_data.json")
     return
 
 def Testing_processing():
+    """Processes the testing dataset"""
     descList=[]
     cur.execute("SELECT Description_File_Path FROM DatasetTesting")
     testList = cur.fetchall()
@@ -128,21 +113,21 @@ def Testing_processing():
     save("encoded_description", "BERT_testing_data.json")
     return
 
-def Gradient_Boosting():
+# def Gradient_Boosting():
     
-    with open('encoded_description/BERT_training_data.json') as f:
-        train_df = json.load(f)
+#     with open('encoded_description/BERT_training_data.json') as f:
+#         train_df = json.load(f)
 
-    with open('encoded_description/BERT_testing_data.json') as f:
-        test_df = json.load(f)
+#     with open('encoded_description/BERT_testing_data.json') as f:
+#         test_df = json.load(f)
 
-    train_df =''
-    test_df = ''
+#     train_df =''
+#     test_df = ''
     
-    return
+#     return
 
 def SVM():
-    
+    """Classifies the dataset through an SVM model"""
     train_df = pd.read_json("encoded_description\BERT_training_data.json", lines=True)
     test_df = pd.read_json("encoded_description\BERT_testing_data.json", lines=True)
     svm = SVC(kernel='poly')
@@ -156,26 +141,23 @@ def SVM():
     svm.fit(train_body, train_labels)
     predicted_test_labels = svm.predict(test_body)
 
+    # Uncomment if on f score is needed
     # metrics = f1_score(test_label, predicted_test_labels, average="weighted")
     # print(metrics) 
     metrics = classification_report(test_labels, predicted_test_labels)
     print(metrics)
 
+    # Saves model for future use 
+    with open('svm_desc.pkl', 'wb') as f:
+        pickle.dump(svm, f)
+
     return
 
 # Training_processing()
 # Testing_processing()
-SVM()
+# SVM()
 
-# Training_processing()
-# Testing_processing()
-# print("Done")
+## Predicts the sponsor within a text
 
-# Load up a bunch of video data to make a dataset
-# Create a dataframe with all the description based data
-# Aspect Extraction:
-# Use Bag of Words?
-# Hashing is a thing?
-# Sentiment Analysis
-
-# SVM, gradient boosting
+def Feature_Extraction():
+    return
